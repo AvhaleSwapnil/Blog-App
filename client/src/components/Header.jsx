edc2bc922bc812c5275c1c7653fa847a1572ad65
AvhaleSwapnil <swapnilavhale460@gmail.com>
2025-01-03 19:36:37 +0530

    create Header Componet

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          Blog <span className="text-primary">App</span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <form className="d-flex mx-auto" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/" className="nav-link active" aria-current="page">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/projects" className="nav-link">
+                Projects
+              </Link>
+            </li>
+            <button type="button" className="btn btn-outline-primary">
+              Sign In
+            </button>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
