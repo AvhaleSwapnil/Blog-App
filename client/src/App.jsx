4c8a5ce02d802ac1df6e536b9a29c1941eb709b9
AvhaleSwapnil <swapnilavhale460@gmail.com>
2025-01-13 18:42:21 +0530

    adding dashboard private functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 import About from "./pages/About";
 import Dashborad from "./pages/Dashborad";
 import Home from "./pages/Home";
@@ -16,7 +17,9 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashborad />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashborad />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
